605e52d871e66cf0ccca4c73bc7f3ef6e3abff28
AstroDev07 <diegoaep07@proton.me>
2023-10-15 17:44:15 -0400

    Nueva opcion para buscar

--- ascii.cpp
+++ ascii.cpp
@@ -116,18 +116,40 @@ int showAsciiSpecChars(){
 
 	return 0;
 }
+//Funcion para que el usuario pueda buscar el caracter deseado
+int search(){
+	std::cout<<"Desea buscar por numero[1] o por caracter[2]? Otra entrada cerrara el programa: ";
+	int opt;
+	std::cin>>opt;
+	if(opt==1){
+		std::cout<<"\nDiga el numero: ";
+		int num;
+		std::cin>>num;
+		std::cout<<"\nEl caracter correspondiente a su numero es: "<<static_cast<char>(num)<<"\n";
+	}
+	else if(opt==2){
+		std::cout<<"\nDiga el caracter: ";
+		char car;
+		std::cin>>car;
+		std::cout<<"\nEl numero correspondiente a su caracter es: "<<static_cast<int>(car)<<"\n";
+	}
+	else{
+		exit(0);
+	}
+	return 0;
+}
 
 int main(){
 	std::cout<<"\n\t\tBienvenido al ASCII Learner\n";
 	std::cout<<"Estas son las opciones disponibles:\n";
 	std::cout<<"\t1-Salir\n\t2-Mostrar las Letras\n\t3-Mostrar caract. espec.\n\t4-Mostrar Todo\n";
-	std::cout<<"\t5-Practicar\n\t6-Limpiar la pantalla(No Salir)\n";
+	std::cout<<"\t5-Practicar\n\t6-Limpiar la pantalla(No Salir)\n\t7-Buscar\n";
 	std::cout<<"Introduzca el numero correspondiente a la opcion deseada:";
 	while(true){
 		int option;
 		std::cin>>option;
 		if(option==1){
-			std::cout<<"\n\tHatsa la Vista";
+			std::cout<<"\n\tHatsa la Vista\n";
 			break;
 		}
 		else if(option==2){
@@ -146,11 +168,14 @@ int main(){
 		else if(option==6){
 			system(CLEAR_COMMAND);
 		}
+		else if(option==7){
+			search();
+		}
 		else{
 			std::cout<<"\vOPCION NO RECONOCIDA\n";
 			std::cout<<"Estas son las opciones disponibles:\n";
 			std::cout<<"\t1-Salir\n\t2-Mostrar las Letras\n\t3-Mostrar caract. espec.\n\t4-Mostrar Todo\n";
-			std::cout<<"\t5-Practicar\n\t6-Limpiar la pantalla(No Salir)\n";
+			std::cout<<"\t5-Practicar\n\t6-Limpiar la pantalla\n\t7-Buscar\n";
 		}
 
 		std::cout<<"Siguiente Comando:";
